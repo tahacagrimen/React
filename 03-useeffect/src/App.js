97bd4ae67a120bc9e5b8dc7a0a9135e2eb6982d3
tahacagrimen <tahacagrimen2@gmail.com>
2022-06-22 01:37:23 +0300

    usereef

--- 03-useeffect/src/App.js
+++ 03-useeffect/src/App.js
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  // useState ile buttonların stateini oluşturuyoruz.
+  // buttonlara bastığımızda state değerini değiştiriyoruz.
+
   const [resourseType, setResourceType] = useState("posts");
+
+  // items api dan çektiğimiz veriyi ayarlamak için
+
+  const [items, setItems] = useState([]);
+
+  // useEffect ilk önce fonksiyonumuzu alıyor. Fetch ile veriyi çektik. [resourseType] ile ise state'in değişip değişmediğini kontrol ediyoruz.
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourseType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourseType]);
 
   return (
     <>
       <div>
-        <button onClick={() => setResourceType("post")}>Posts</button>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
         <button onClick={() => setResourceType("users")}>Users</button>
         <button onClick={() => setResourceType("comments")}>Comments</button>
       </div>
       <h1>{resourseType}</h1>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
     </>
   );
 }
